27b1372f70e244a45ce8cc8cf15557a7eca8d591
Niranjan Thilakaratna <nirandhammika@gmail.com>
2024-09-12 23:40:26 +0530

    remove unwanted fields in section

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.section(section_name, deleted)
+	VALUES ('IELTS',false);
+INSERT INTO public.section(section_name, deleted)
+	VALUES ('PTE',false);
+INSERT INTO public.section(section_name, deleted)
+	VALUES ('OET',false);
+
+--INSERT INTO public.course(
+--	amount,installment_count, is_installment, section_id, teacher_teacher_id, created_at, update_at, category, created_by, name, updated_by)
+--	VALUES (100.00, 2, true, 1, null, '2024-08-19 17:03:27.852801', null, null, 'Speaking', 'IELTS-Speaking', null);
